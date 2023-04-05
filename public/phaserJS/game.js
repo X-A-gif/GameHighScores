ee30361adba76038c1f53495b3d2637b8794e9af
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-04-04 18:31:55 -0700

    space invader setup

--- public/phaserJS/game.js
+++ public/phaserJS/game.js
@@ -6,6 +6,9 @@ var config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
+  physics: {
+    default: 'arcade'
+  },
   scene: {
       preload: preload,
       create: create,
@@ -13,17 +16,43 @@ var config = {
   }
 };
 
-var game = new Phaser.Game(config);
+
+let game = new Phaser.Game(config);
 
 function preload ()
 {
   this.load.image('background', 'assets/invadersbg.png');
+  this.load.image('ship', 'assets/spaceShips_001.png');
 }
+
+let ship;
 
 function create ()
 {
-  var background = this.add.image(game.config.width / 2, game.config.height / 2, 'background');
+  let background = this.add.image(game.config.width / 2, game.config.height / 2, 'background');
   background.setOrigin(0.5, 0.5);
+
+  ship = this.physics.add.sprite(game.config.width / 2, game.config.height - 100, 'ship');
+  ship.setScale(0.5, -0.5);
+  ship.setOrigin(0.5, 1);
+  
+  ship.setCollideWorldBounds(true);
+
+  let cursors = this.input.keyboard.createCursorKeys();
+
+  cursors.left.on('down', function() {
+    ship.setVelocityX(-200);
+  });
+  cursors.right.on('down', function() {
+    ship.setVelocityX(200);
+  });
+  cursors.left.on('up', function() {
+    ship.setVelocityX(0);
+  });
+  cursors.right.on('up', function() {
+    ship.setVelocityX(0);
+  });
+
 }
 
 function update ()
